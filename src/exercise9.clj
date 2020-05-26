76fc6c2d4890ade7c6271623a9304dcdbf65ab6c
Jorge Magnus <ancajorgel@yahoo.com.ar>
2020-05-26 17:11:27 -0300

    agrego solucion ejercicio 9

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,9 +1,16 @@
 (ns exercise9)
 
+
+(defn filter-letters
+[x]
+(apply str (filter #(or (Character/isLetter %) (= % \space )  (Character/isDigit %) ) x)))
+
+
 (defn word-count 
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (throw (Exception. "Not implemented."))
+  (frequencies (clojure.string/split  (clojure.string/lower-case  (filter-letters x)) #"\s+"))
+
 )
 
 
